4dd6acc1c2c023ab02590e478bfa668214ad85b4
Christopher Maahs <cmaahs@splicemachine.com>
2020-09-30 23:43:21 -0500

    Try another way

--- Formula/sqlshell.rb
+++ Formula/sqlshell.rb
@@ -4,16 +4,55 @@ class Sqlshell < Formula
   url "https://splice-releases.s3.amazonaws.com/3.0.0.1960/cluster/sqlshell/sqlshell-3.0.0.1960.tar.gz"
   sha256 "a7a8b999a69f1e75c80239e2786ff7eb8d6faddd76ffc0ffe4f07cf32880d2a7"
   version "3.0.0.1960"
-  revision 9
+  revision 10
 
   depends_on "coreutils"
 
-  resource "sources" do
-    url "https://github.com/splicemachine/homebrew-utility.git"
-  end
-
   def install
-    resource("sources").stage { system "patch", "./sqlshell.sh", "./bin/sqlshell.patch" }
+    (patch/"sqlshell.patch").write <<~EOF
+      *** sqlshell.sh	2020-06-17 21:29:40.000000000 -0500
+      --- /usr/local/bin/sqlshell.sh	2020-09-30 22:52:16.000000000 -0500
+      ***************
+      *** 153,165 ****
+        SPLICE_LIB_DIR="##SPLICELIBDIR##"
+
+        if [[ "$SPLICE_LIB_DIR" == *"##"* ]]; then
+      !    CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+      !
+      !    # ends in bin, go up and over to lib
+      !    if [[ "$CURDIR" = *"bin" ]]; then
+      !       SPLICE_LIB_DIR="${CURDIR}/../lib"
+      !    else #subdir
+      !       SPLICE_LIB_DIR="${CURDIR}/lib"
+          fi
+        fi
+
+      --- 153,175 ----
+        SPLICE_LIB_DIR="##SPLICELIBDIR##"
+
+        if [[ "$SPLICE_LIB_DIR" == *"##"* ]]; then
+      !    SYMLINK_TARGET_DIR=$(dirname $(realpath $0))
+      !    if [[ -z ${SYMLINK_TARGET_DIR} ]]; then
+      !       CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+      !
+      !       # ends in bin, go up and over to lib
+      !       if [[ "$CURDIR" = *"bin" ]]; then
+      !          SPLICE_LIB_DIR="${CURDIR}/../lib"
+      !       else #subdir
+      !          SPLICE_LIB_DIR="${CURDIR}/lib"
+      !       fi
+      !    else
+      !       # ends in bin, go up and over to lib
+      !       if [[ "${SYMLINK_TARGET_DIR}" = *"bin" ]]; then
+      !          SPLICE_LIB_DIR="${SYMLINK_TARGET_DIR}/../lib"
+      !       else #subdir
+      !          SPLICE_LIB_DIR="${SYMLINK_TARGET_DIR}/lib"
+      !       fi
+          fi
+        fi
+
+    EOF
+    system "patch sqlshell.sh patch/sqlshell.patch"
     bin.install "sqlshell.sh"
     lib.install Dir["lib/*.*"]
   end
