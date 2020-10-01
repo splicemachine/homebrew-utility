29e9ae55e1235c948e5bd4cbd9d0dfbc4da8510b
Christopher Maahs <cmaahs@splicemachine.com>
2020-09-30 23:53:19 -0500

    Debug 1

--- Formula/sqlshell.rb
+++ Formula/sqlshell.rb
@@ -4,7 +4,7 @@ class Sqlshell < Formula
   url "https://splice-releases.s3.amazonaws.com/3.0.0.1960/cluster/sqlshell/sqlshell-3.0.0.1960.tar.gz"
   sha256 "a7a8b999a69f1e75c80239e2786ff7eb8d6faddd76ffc0ffe4f07cf32880d2a7"
   version "3.0.0.1960"
-  revision 13
+  revision 14
 
   depends_on "coreutils"
 
@@ -52,7 +52,7 @@ class Sqlshell < Formula
         fi
 
     EOS
-    system "patch sqlshell.sh sqlshell.patch"
+    # system "patch sqlshell.sh sqlshell.patch"
     bin.install "sqlshell.sh"
     lib.install "sqlshell.patch"
     lib.install Dir["lib/*.*"]
