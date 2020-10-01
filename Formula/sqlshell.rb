ccdfed82e1867baa4c035c424da6fa588a1865b4
Christopher Maahs <cmaahs@splicemachine.com>
2020-09-30 22:02:10 -0500

    Install libraries

--- Formula/sqlshell.rb
+++ Formula/sqlshell.rb
@@ -4,10 +4,11 @@ class Sqlshell < Formula
   url "https://splice-releases.s3.amazonaws.com/3.0.0.1960/cluster/sqlshell/sqlshell-3.0.0.1960.tar.gz"
   sha256 "a7a8b999a69f1e75c80239e2786ff7eb8d6faddd76ffc0ffe4f07cf32880d2a7"
   version "3.0.0.1960"
-  revision 3
+  revision 4
 
   def install
     bin.install "sqlshell.sh"
+    lib.install Dir["lib/*.*"]
   end
 
 def caveats; <<~EOS
